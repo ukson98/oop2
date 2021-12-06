f116ddeb2e3d495406489017e7870f1a154e0d55
UKSON <ukson@naver.com>
2021-12-06 15:34:03 +0900

    v2

---
+++ hello.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<fstream>
+
+using namespace std;
+
+int main(){
+	int m = 0;
+	cout<<"hello world"<<endl;
+	try{
+		cin>>m;
+	}
+	catch(...){
+	}
+	cout<<"endl"<<endl;
+	return 0;
+}
